751ed629e81ccba06b3d5c3d2a6b342219c29a9f
Alessandro <alessandrovaru@gmail.com>
2024-11-27 19:27:46 -0400

    Enhance admin login page with improved layout, styling, and accessibility features

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,12 +1,26 @@
+import Image from 'next/image'
 import { login } from './actions'
 export default function AdminPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-    </form>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src="/images/logo-jonron.png"
+        alt="Pepsi Pulse Logo"
+        width={200}
+        height={200}
+        className='mx-auto mt-10'
+      />
+      <form className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md font-[family-name:var(--font-pepsi-owners-2-compressed)]">
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email:</label>
+          <input id="email" name="email" type="email" required className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black" />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password:</label>
+          <input id="password" name="password" type="password" required className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black" />
+        </div>
+        <button formAction={login} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#0025ff] hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Log in</button>
+      </form>
+    </div>
   )
 }
