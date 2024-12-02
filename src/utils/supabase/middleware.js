fbb26dd2210c5137c1ac2b57a3394e08705bf887
Alessandro <alessandrovaru@gmail.com>
2024-12-02 12:02:02 -0500

    Remove unnecessary path checks in session update middleware for cleaner logic

--- src/utils/supabase/middleware.js
+++ src/utils/supabase/middleware.js
@@ -32,7 +32,7 @@ export async function updateSession(request) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (!user && !request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/auth') && request.nextUrl.pathname !== '/' && !request.nextUrl.pathname.startsWith('/quiniela') ) {
+  if (!user && !request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/auth') && request.nextUrl.pathname !== '/' ) {
     // No user, redirect to login page
     const url = request.nextUrl.clone()
     url.pathname = '/login'
