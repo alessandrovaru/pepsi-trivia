1c56159140f631594b8fb659a5070a06e551410c
Alessandro <alessandrovaru@gmail.com>
2024-12-02 13:35:25 -0500

    Add underline style to footer link for improved visibility

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="footer flex justify-center items-center bg-white h-[4dvh]">
       <div className="container flex justify-center items-center">
-        <span className="font-[family-name:var(--font-pepsi-owners-2-compressed)] text-black text-xs">Made with love for pepsi  &#160;&copy; by <a href='https://www.alessandrovaru.com' target="_blank">alessandrovaru</a> </span>
+        <span className="font-[family-name:var(--font-pepsi-owners-2-compressed)] text-black text-xs">Made with love for pepsi  &#160;&copy; by <a href='https://www.alessandrovaru.com' target="_blank" className="underline">alessandrovaru</a> </span>
       </div>
     </footer>
   )
